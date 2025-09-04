4a0df9e19b6e4ae5a5dfd84835146515011b888f
wjzcd2011 <36251173+wjzcd2011@users.noreply.github.com>
2025-09-04 11:51:02 +0800

    Upload by PicGo

---
+++ img/he-standard-common2.0.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://cdn.lpxyr0206.com/img/he-standard.css'
+  var s = d.createElement('script')
+  s.src = 'https://cdn.lpxyr0206.com/img/he-standard-common14.js'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
